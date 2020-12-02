c8ae5d7b24510536b793b067f67f727f314a9578
fionn-oreilly <55212355+fionn-oreilly@users.noreply.github.com>
2020-12-02 13:53:46 +0000

    Create index.js

---
+++ index.js
@@ -0,0 +1,19 @@
+<!DOCTYPE HTML>
+
+<html>
+  <head>
+    <title>React!</title>
+    <script crossorigin src="https://unpkg.com/react16/umd/react.production.min.js"></script>
+    <script crossorigin src="https://unpkg.com/react-dom16/umd/react-dom.production.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/babel-standalone/6.24.0/babel.js"></script>
+   </head>
+   
+   <body>
+    <script type="text/babel">
+      ReactDOM.render(
+        <h1>Sherlock Holmes</h1>
+        document.body
+       );
+      </script>
+     </body>
+    </html>
